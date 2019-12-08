9cb2c7c47a673a96325a317608b9f64c73d83dbd
gonzalo moreno arevalo <gonzalomoreno05@gmail.com>
2019-12-08 19:02:32 +0100

    Practica 4 - Pokedex

---
+++ Parctica 4/Practica 4 Pokedex/Practica 4 Pokedex/TypesTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  TypesTableViewCell.swift
+//  Practica 4 Pokedex
+//
+//  Created by g834 DIT UPM on 5/11/18.
+//  Copyright © 2018 g834 DIT UPM. All rights reserved.
+//
+
+import UIKit
+
+class TypesTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var iconImageView: UIImageView!
+    
+
+}
---
+++ Parctica 4/Practica 4 Pokedex/Practica 4 PokedexTests/Practica_4_PokedexTests.swift
@@ -0,0 +1,36 @@
+//
+//  Practica_4_PokedexTests.swift
+//  Practica 4 PokedexTests
+//
+//  Created by g834 DIT UPM on 5/11/18.
+//  Copyright © 2018 g834 DIT UPM. All rights reserved.
+//
+
+import XCTest
+@testable import Practica_4_Pokedex
+
+class Practica_4_PokedexTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
