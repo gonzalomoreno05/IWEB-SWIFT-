4d7c7ed316ce3f0821758b68a8d665f7d8989e06
gonzalo moreno arevalo <gonzalomoreno05@gmail.com>
2019-12-08 18:57:23 +0100

    Practica 2 -Cubo Flotando

---
+++ Practica 2/P2.1/IWEB P2/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  IWEB P2
+//
+//  Created by g834 DIT UPM on 18/9/18.
+//  Copyright © 2018 g834. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var sliderLado: UISlider!
+    @IBOutlet weak var sliderTiempo: UISlider!
+    
+        override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+   
+    
+  }
+
