a0d3056a5257e070b53d2708899e5b8d375786bc
gonzalo moreno arevalo <gonzalomoreno05@gmail.com>
2019-12-08 19:03:33 +0100

    Add files via upload

---
+++ P5_Quiz/P5_Quiz/TriviaTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaTableViewCell.swift
+//  P5_Quiz
+//
+//  Created by g834 DIT UPM on 12/12/18.
+//  Copyright © 2018 g834. All rights reserved.
+//
+
+import UIKit
+
+class TriviaTableViewCell: UITableViewCell {
+
+
+    @IBOutlet weak var AutorLabel: UILabel!
+    @IBOutlet weak var TriviaLabel: UILabel!
+    @IBOutlet weak var TriviaImagen: UIImageView!
+}
---
+++ P5_Quiz/P5_Quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  P5_Quiz
+//
+//  Created by g834 DIT UPM on 12/12/18.
+//  Copyright © 2018 g834. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
